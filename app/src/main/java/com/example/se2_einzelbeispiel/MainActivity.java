4aef6d481a2a9f7af09f8c177d00655088fcebba
Benjamin Dippold <dippold.benjamin@gmail.com>
2023-03-11 19:21:59 +0100

    Socket Implementation

--- app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/se2_einzelbeispiel/MainActivity.java
@@ -2,38 +2,62 @@ package com.example.se2_einzelbeispiel;
 
 import android.os.Bundle;
 
+import com.example.se2_einzelbeispiel.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.se2_einzelbeispiel.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    final String IP = "se2-isys.aau.at";
+    final int PORT = 53212;
+    private String response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.se2_einzelbeispiel.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        binding.btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(() -> {
+                    try {
+                        Socket soc = new Socket(IP, PORT);
+                        OutputStream out = soc.getOutputStream();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                        EditText txtInput = findViewById(R.id.txtInput);
+                        String message = txtInput.getText().toString();
+                        out.write(message.getBytes());
+                        out.write("\n".getBytes());
+
+                        response = in.readLine();
+                        TextView txtResponse = findViewById(R.id.txtServerAnswer);
+                        txtResponse.post(() -> txtResponse.setText(response));
+
+                        in.close();
+                        out.close();
+                        soc.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            }
+        });
     }
 }
